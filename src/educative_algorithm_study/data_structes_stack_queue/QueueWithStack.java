7ae032efee399bfc6be8b7dce21b438158c5565a
ThugsInSuits <1036023528@qq.com>
2023-11-25 17:40:06 +0800

    stack

---
+++ src/educative_algorithm_study/data_structes_stack_queue/QueueWithStack.java
@@ -0,0 +1,37 @@
+package educative_algorithm_study.data_structes_stack_queue;
+
+/**
+ * @author XiZhuangBaoTu
+ * Date 2023/11/25 17:26
+ * Version 1.0
+ * @description
+ **/
+public class QueueWithStack<V> {
+    private Stack<V> stack1;
+    private Stack<V> stack2;
+
+    public QueueWithStack(int maxSize){
+        stack1 = new Stack<>(maxSize);
+        stack2 = new Stack<>(maxSize);
+    }
+
+    public boolean isEmpty( ){
+        return stack2.isEmpty();
+    }
+
+    public void enqueue(V value) {
+      stack1.push(value);
+    }
+
+    public V dequeue() {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+
+        V temp = stack2.pop();
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+        return temp;
+    }
+}
---
+++ src/educative_algorithm_study/data_structes_stack_queue/Stack.java
@@ -0,0 +1,55 @@
+package educative_algorithm_study.data_structes_stack_queue;
+
+/**
+ * @author XiZhuangBaoTu
+ * Date 2023/11/25 13:30
+ * Version 1.0
+ * @description
+ **/
+public class Stack<V> {
+    private int maxSize;
+    private int top;
+    private V[] arr;
+
+    @SuppressWarnings("unchecked")
+    public Stack(int max_size){
+        this.maxSize = max_size;
+        this.top = -1;
+        arr = (V[]) new Object[max_size];
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public boolean isEmpty(){
+        return top == -1;
+    }
+
+    public boolean isFull(){
+        return top == maxSize - 1;
+    }
+
+    public V top(){
+        if (isEmpty()) {
+            return null;
+        }
+        return arr[top];
+    }
+
+    public void push(V value) {
+        if (isFull()) {
+            System.out.println("This stack is full");
+            return;
+        }
+        arr[++top] = value;
+    }
+
+    public V pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        return arr[top--];
+    }
+
+}
