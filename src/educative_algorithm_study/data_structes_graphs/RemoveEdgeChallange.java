5d2cc5f53bf9135057521d7a86904cfb0cc18e3e
ThugsInSuits <1036023528@qq.com>
2023-11-28 13:53:06 +0800

    RemoveEdgeChallange

---
+++ src/educative_algorithm_study/data_structes_graphs/RemoveEdgeChallange.java
@@ -0,0 +1,29 @@
+package educative_algorithm_study.data_structes_graphs;
+
+/**
+ * @author XiZhuangBaoTu
+ * Date 2023/11/28 13:44
+ * Version 1.0
+ * @description
+ **/
+public class RemoveEdgeChallange {
+    public  static void removeEdge(Graph g,int source,int destination) {
+        g.adjacencyList[source].deleteByValue(destination);
+    }
+
+    public static void main(String args[]) {
+        Graph g=new Graph(5);
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        g.addEdge(1,3);
+        g.addEdge(3,4);
+        g.addEdge(1,4);
+
+        System.out.println("Before:");
+        g.printGraph();
+
+        removeEdge(g, 1, 3);
+        System.out.println("After:");
+        g.printGraph();
+    }
+}
